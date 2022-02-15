f2add734e8955daf7e847c11bb7adbac84e8f470
Stacy Jung <stacyj1417@gmail.com>
2022-02-14 16:33:33 -0800

    Basic Card component with background image and stats display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Card from './Components/Card';
 function App() {
-  return <div className='App'>hello world</div>;
+  return (
+    <div className='App'>
+      <Card values={[1, 3, 'A', 'A']} />
+      <Card values={[6, 6, 6, 6]} />
+      <Card values={[0, 1, 2, 3]} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '../styles/Card.module.css';
+import img from '../images/FatCatHeart.png';
+
+export default function Card({ values }) {
+  //const stats = [1, 2, 3, 4];
+  const valuePositions = ['N', 'W', 'E', 'S'];
+  return (
+    <div className={styles.container}>
+      <div>test</div>
+      {/* <img src={img} alt='card-art' /> */}
+      <div className={styles.values}>
+        {values.map((stat, index) => (
+          <div key={index} className={styles[valuePositions[index]]}>
+            {stat}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
