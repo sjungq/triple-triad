7ae940127e3e9dc63cc4f23e8343a021c7801429
Stacy Jung <stacyj1417@gmail.com>
2022-06-10 15:45:58 -0700

    Moved all card and tile colors to sass variables

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/Card.module.css';
-//import img from '../images/FatCatHeart.png';
 
 export default function Card({
   values,
@@ -12,18 +11,15 @@ export default function Card({
   index,
   flipped,
 }) {
-  //const stats = [1, 2, 3, 4];
   const valuePositions = ['N', 'W', 'E', 'S'];
 
   const style = {
     backgroundImage: `url(/images/${url})`,
-    backgroundColor: owner,
   };
 
   const flipStyle = flipped ? styles.flipped : '';
 
   const cardClick = (evt) => {
-    //flipOwner();
     if (setActiveCard) {
       setActiveCard(index);
     } else {
@@ -33,13 +29,15 @@ export default function Card({
   return (
     <div
       style={style}
-      className={`${styles.container} ${flipStyle}`}
+      className={
+        `${styles.container} ${flipStyle} ` +
+        (owner === 'BLUE' ? styles.BLUE : styles.RED)
+      }
       onClick={cardClick}
       title={name}
       key={`${owner}${index}`}
     >
       <div className={styles.rank}>{rank}</div>
-      {/* <img src={`${process.env.PUBLIC_URL}/images/${url}`} alt='card-art' /> */}
       <div className={styles.values}>
         {values.map((stat, index) => (
           <div key={index} className={styles[valuePositions[index]]}>
